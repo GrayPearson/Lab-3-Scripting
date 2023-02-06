0626caa7ca02127c84de9a595d5a5a5236434b3e
Gray Pearson <122818113+GrayPearson@users.noreply.github.com>
2023-02-06 13:25:08 -0500

    Created the first 2 fucntions in the lab

--- lab3_script_template.py
+++ lab3_script_template.py
@@ -1,3 +1,7 @@
+import sys
+import os
+from datetime import date
+import pandas as pd
 
 def main():
     sales_csv = get_sales_csv()
@@ -7,19 +11,41 @@ def main():
 # Get path of sales data CSV file from the command line
 def get_sales_csv():
     # Check whether command line parameter provided
-    # Check whether provide parameter is valid path of file
-    return
+    num_params = len(sys.argv) - 1
+    if num_params >= 1:
+        csv_path = sys.argv[1]    
+        # Check whether provide parameter is valid path of file
+        if os.path.isfile(csv_path):
+            return os.path.abspath(csv_path)
+        else:
+            print("Error: File Does Not Exist")
+            sys.exit(1)
+
+    else:
+        print('Error: Missing File Path.')
+        sys.exit(1)
 
 # Create the directory to hold the individual order Excel sheets
 def create_orders_dir(sales_csv):
     # Get directory in which sales data CSV file resides
+    sales_dir = os.path.dirname(sales_csv)
     # Determine the name and path of the directory to hold the order data files
+    todays_date = date.today().isoformat()
+    orders_dir_name = f"Orders_{todays_date}"
+    orders_dir_path = os.path.join(sales_dir, orders_dir_name)
     # Create the order directory if it does not already exist
-    return 
+    if not os.path.isdir(orders_dir_path):
+        os.makedirs(orders_dir_path)
+
+    return orders_dir_path
 
 # Split the sales data into individual orders and save to Excel sheets
 def process_sales_data(sales_csv, orders_dir):
     # Import the sales data from the CSV file into a DataFrame
+ 
+
+
+
     # Insert a new "TOTAL PRICE" column into the DataFrame
     # Remove columns from the DataFrame that are not needed
     # Group the rows in the DataFrame by order ID
